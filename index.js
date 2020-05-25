7da35eaaa2f48c8d214e0659a31f213d38c30b6b
Fajar Firdaus <35714197+FajarTheGGman@users.noreply.github.com>
2020-05-25 21:33:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -65,12 +65,13 @@ ask.prompt({
 
 
 // Tracking Function
+
+function tracking(){
+
 setInterval(() => {
     consoke.log(warna.bgBlue("[!] Please wait.."))
 }, 5000)
-function tracking(){
-
-
+    
 console.log(warna.bgBlue("[Link] : ") + warna2.rainbow(s.exec("curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o 'https://[0-9a-z]*\.ngrok.io'")))
 
 
